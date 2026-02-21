3145eaf62f7b2004f04edcc1a427489151a3b8b8
Ozean <2397262583@qq.com>
2026-02-21 20:32:47 +0800

    initialization

---
+++ src/main/java/com/ozean/ku/KuApplication.java
@@ -0,0 +1,13 @@
+package com.ozean.ku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuApplication.class, args);
+	}
+
+}
