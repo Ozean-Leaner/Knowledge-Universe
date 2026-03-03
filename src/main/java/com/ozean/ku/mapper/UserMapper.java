af8af91990c61c7b204cd28f9fa33b459ace04b8
Ozean <2397262583@qq.com>
2026-03-03 23:30:12 +0800

    添加部分user修改功能

--- src/main/java/com/ozean/ku/mapper/UserMapper.java
+++ src/main/java/com/ozean/ku/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.ozean.ku.VO.UserSimpleVO;
 import com.ozean.ku.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -30,4 +31,16 @@ public interface UserMapper {
 
     @Select("select user_id, username, description, gender, post_id, avatar, comment_id from user where user_id = #{id}")
     UserDetailVO getUserDetailInfo(String id);
+
+    @Select("select user_id, username, gender, avatar, description from user where user_id = #{id}")
+    UserSimpleVO getUserSimpleInfo(String id);
+
+    @Update("update user set last_login_time = NOW() where user_id = #{id} ")
+    void updateUserLoginTime(String id);
+
+    @Update("update user set username = #{username}, email = #{email}, gender = #{gender}, description = #{desc}, avatar = #{avatar} where user_id = #{id}")
+    User updateUserById(String id, String username, String email, Integer gender, String desc, String avatar);
+
+    @Update("update user set password = #{password} where user_id = #{id}")
+    void updateUserPwdById(String id, String password);
 }
